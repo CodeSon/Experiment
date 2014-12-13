f8f21194eb0c7abcfbfa5b486a70f79e4e33019a
David Nartey <david.nartey.com>
2014-12-13 19:30:59 +0100

    Subtruction possibility added to the code
    Made it possible to carry out subtaruction as well.

--- src/ExperimentCalc.java
+++ src/ExperimentCalc.java
@@ -24,90 +24,83 @@ public class ExperimentCalc {
 		// addera heltalen och visa resultatet
 		String summa = addera(tal1, tal2);
 		visa(tal1, tal2, summa, '+');
+		/**
+		 * A measure to ensure that the value of the first string is not less
+		 * than the value of the second string. Checking whether the first
+		 * answer from the calculation is a negative so as to exit the program
+		 * in compliance with assignment.
+		 */
+		if (Integer.parseInt(tal1) - Integer.parseInt(tal2) < 0) {
+			System.out
+					.println("To carry out subtruction, the value of the first string\n"
+							+ " shouldn't be less than the value of the second string. Try again");
+			System.exit(0);
+		}
 
-		// subtrahera heltalen och visa resultatet
+		// Subtracting the numbers and showing their results.
 		String diferens = subtrahera(tal1, tal2);
 		visa(tal1, tal2, diferens, '-');
-		
-		/*multiplikation
-		String multi= multiplicera(tal1,tal2);
-		visa(tal1,tal2,'*');*/
 
+		/*
+		 * multiplicera heltalen och visa resultatet String multi =
+		 * subtrahera(tal1, tal2); visa(tal1, tal2, diferens, '-');
+		 */
 	}
 
-	// addera tar emot två naturliga heltal givna som teckensträngar, och
-	// returnerar deras
-	// summa som en teckensträng.
+	/**
+	 * addera receives two natural numbers entered as strings and returns their
+	 * Value
+	 * 
+	 * @param tal1
+	 * @param tal2
+	 * @return
+	 */
 	public static String addera(String tal1, String tal2) {
 
 		String resultat = new String();
 
+		// function call and a way to ensure that both strings are of the same
+		// length.
 		if (tal1.length() > tal2.length()) {
 			tal2 = addZeros(tal2, tal1.length());// funktion anrop
 		} else if (tal2.length() > tal1.length()) {
 			tal1 = addZeros(tal1, tal2.length());
 		}
 
-		// addera innehållet i strängarna
+		// filling the contents of the string
 		for (int i = 0; i < tal1.length(); i++) {
-			// omvandla tecken till siffror
+			// Conversion from character to integer
 			int talett = Character.getNumericValue(tal1.charAt(i));
 			int taltva = Character.getNumericValue(tal2.charAt(i));
 
 			int svar = talett + taltva;
 
 			if (svar > 9) {
-				if (resultat.length() == 0)// Kolla om denna är den första
-											// tecknet
+				if (resultat.length() == 0)// Checking if this is the first
+											// digit
+
 				{
-					resultat = resultat + Integer.toString(svar);// omvändla
-																	// från
-																	// sifra
-																	// till
-																	// sträng
+					resultat = resultat + Integer.toString(svar);// Conversion
+																	// from
+																	// integer
+																	// to string
 
 				} else {
-					resultat = lendDigits(resultat, i, svar);
+					resultat = lendDigits2(resultat, i, svar);
 				}
 
 			} else
 
 			{
-				resultat = resultat + Integer.toString(svar);// från sifra till
-																// sträng
-			}
+				resultat = resultat + Integer.toString(svar);// Conversion from
+																// integer to
+																// string
+}
 
 		}
 
 		return resultat;
 
-	}
-
-	/**
-	 * 
-	 * 
-	 * @param resultat
-	 *            the string with the preliminary result
-	 * @param i
-	 *            the current position of the loop
-	 * @param svar
-	 *            the result of the addition of the current digits
-	 * @return the result of the calculation
-	 */
-	private static String lendDigits(String resultat, int i, int svar) {
-		int previousDigit = Character.getNumericValue(resultat.charAt(i - 1));// kontrolera
-																				// det
-		// förengående
-		// tecknet
-		System.out.println("previousDigit: " + previousDigit);
-		previousDigit = previousDigit + 1;
-		System.out.println("previousDigit = previousDigit + 1; "
-				+ previousDigit);//
-
-		System.out.println("resultat: " + resultat);
-		resultat = resultat.substring(0, i - 1) + previousDigit + (svar - 10);
-		System.out.println("resultat after: " + resultat);
-		return resultat;
 	}
 
 	/**
@@ -118,10 +111,8 @@ public class ExperimentCalc {
 	 *            the target length
 	 * @return the number tal but padded with zeroes in front
 	 */
-	private static String addZeros(String tal, int length) // lägga till nollor
-															// tills tal har
-															// samma längd som
-															// length
+	private static String addZeros(String tal, int length)
+
 	{
 		while (tal.length() < length) {
 			tal = "0" + tal;
@@ -132,54 +123,51 @@ public class ExperimentCalc {
 
 	}
 
-	// subtrahera tar emot två naturliga heltal givna som teckensträngar, och
-	// returnerar
-	// deras differens som en teckensträng.
-	// Det första heltalet är inte mindre än det andra heltalet.
+	/**
+	 * subtrahera receives two natural numbers as strings and returns
+	 * 
+	 * @param tal1
+	 * @param tal2
+	 * @return their difference as a string
+	 */
 	public static String subtrahera(String tal1, String tal2) {
 
-
 		String resultat = new String();
-
+		
 		if (tal1.length() > tal2.length()) {
+
 			tal2 = addZeros(tal2, tal1.length());// funktion anrop
 		} else if (tal2.length() > tal1.length()) {
 			tal1 = addZeros(tal1, tal2.length());
 		}
-
+		
+		
 		// subtrahera innehållet i strängarna
-		for (int i = 0; i < tal1.length(); i++) {
-			// omvandla tecken till siffror
-
+		// omvandla tecken till siffror
+		int lenddigit = 0;
+		for (int i = (tal1.length()-1); 0 <= i ; i--) {
 			int talett = Character.getNumericValue(tal1.charAt(i));
 			int taltva = Character.getNumericValue(tal2.charAt(i));
-
-			int svar = talett - taltva;
-
-			if (svar > 9)
+			int svar = 0;
+			
+			if (talett==0 & lenddigit == 1)//If digit=0 and has borrowed(1) previousely, make digit = 9
 			{
-				if (resultat.length() == 0)// Kolla om denna är den första
-											// tecknet
-				{
-					resultat = resultat + Integer.toString(svar);// omvändla
-																	// från
-																	// sifra
-																	// till
-																	// sträng
-
-				} 
-				else {
-					resultat = lendDigits(resultat, i, svar);
-				}
-
-			} 
-			else 
-				
-			{
-				resultat = resultat + Integer.toString(svar);// från sifra till
-																// sträng
+				talett = 9;
+				svar = talett - taltva;//9 - taltva
+				lenddigit = 1;
+			// otherwise check if talett is less than taltva, and if so, add 10 to talett then lenddigit = 1
+			} else if ((talett-lenddigit) < taltva) {
+				talett = talett + 10;
+				svar = talett - taltva;
+				lenddigit = 1;
+			// otherwise just take the difference between  talett(minus lenddigit if there is one) and taltva
+			// lenddigit  = 0 since no digitis were borrowed 
+			} else {
+				svar=(talett-lenddigit)-taltva;
+				lenddigit = 0;
 			}
-
+			// add the svar digit to the resultat string
+				resultat =  Integer.toString(svar)+resultat ;
 		}
 
 		return resultat;
@@ -198,18 +186,14 @@ public class ExperimentCalc {
 	 * @return the result of the calculation
 	 */
 	private static String lendDigits2(String resultat, int i, int svar) {
-		int previousDigit = Character.getNumericValue(resultat.charAt(i - 1));// kontrolera
-																				// det
-																				// förengående
-																				// tecknet
+		int previousDigit = Character.getNumericValue(resultat.charAt(i - 1));// Checking
+																				// the
+																				// previous
+																				// digit
+
 		System.out.println("previousDigit: " + previousDigit);
 		previousDigit = previousDigit + 1;
-		System.out.println("previousDigit = previousDigit + 1; "
-				+ previousDigit);
-
-		System.out.println("resultat: " + resultat);
 		resultat = resultat.substring(0, i - 1) + previousDigit + (svar - 10);
-		System.out.println("resultat after: " + resultat);
 		return resultat;
 	}
 
@@ -221,11 +205,7 @@ public class ExperimentCalc {
 	 *            the target length
 	 * @return the number tal but padded with zeroes in front
 	 */
-	private static String addZeros2(String tal, int length) // lägga till nollor
-															// tills tal har
-															// samma längd som
-															// length
-	{
+	private static String addZeros2(String tal, int length) {
 		while (tal.length() < length) {
 			tal = "0" + tal;
 
@@ -234,18 +214,12 @@ public class ExperimentCalc {
 		return tal;
 
 	}
+
 	/**
+	 * public static String multiplicera(String tal1, String tal2) {
+	 * 
+	 * return ""; }
 	 */
-	public static String multiplicera(String tal1, String tal2){
-		
-		
-		
-		
-		
-		return"";
-	}
-	
-	
 
 	// visa visar två givna naturliga heltal, och resultatet av en aritmetisk
 	// operation
@@ -261,13 +235,13 @@ public class ExperimentCalc {
 		tal2 = sattLen(tal2, maxLen - len2);
 		resultat = sattLen(resultat, maxLen - len);
 		// visa heltalen och resultatet
-		out.println(" " + tal1);
+		out.println("  " + tal1);
 		out.println("" + operator + " " + tal2);
-
 		for (int i = 0; i < maxLen + 2; i++)
 			out.print("-");
 		out.println();
-		out.println(" " + resultat + "\n");
+		out.println("  " + resultat + "\n");
+	
 	}
 
 	// sattLen lägger till ett angivet antal mellanslag i början av en given
